4d31d467dad9e70858b72b451d076a8753388a0c
BillySmithDesign <josh@bullsharkinvestments.com>
2024-06-27 23:16:26 +0930

    1.4

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-  },
 };
